b9fe3e8e2a99df500253602e929b42e73060fb1a
Terence Ateya <ateyaterence@gmail.com>
2021-08-20 06:37:52 +0000

    Added a comment on Boostrap.php in order to perform integration test

--- application/test/Bootstrap.php
+++ application/test/Bootstrap.php
@@ -65,7 +65,7 @@ if ( ! defined('ENVIRONMENT'))
  *---------------------------------------------------------------
  * ERROR REPORTING
  *---------------------------------------------------------------
- *
+ *Adding this comment inorder to perform integration testing
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
